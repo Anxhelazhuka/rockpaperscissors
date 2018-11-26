da03bb3e7e024843a2ba0199a7534046e8acfbed
Anxhela Zhuka <anxhela.zhuka@mihsnyc.org>
2018-11-26 23:05:42 +0000

    RPS day 3

--- js/script.js
+++ js/script.js
@@ -8,20 +8,57 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-var userChoice;
-userChoice="";
+
 
 $("#shoot").click(function(){
     var userChoice = $("#input").val();
-
     $("#userChoice").text(userChoice);
 });
+var userChoice;
+var userChoice= "rock";
+var userChoice= "paper";
+var userChoice= "scissors";
 
 var computerChoice;
 computerChoice="";
 
 var winner;
 winner="";
+
+$("#shoot").click(function(){
+ var userChoice = $("#userChoice").val();
+ var computerChoice = $("#computerChoice").val();
+ var winner = $("#result").val();
+   
+   if(computerChoice > .888  && userChoice==="rock") {
+        $("#result").html("There is a tie"); 
+    }
+    else  if(computerChoice > .888 && userChoice==="paper") {
+        $("#result").html("User wins!");  
+    }
+     else  if(computerChoice > .888 && userChoice==="scissors") {
+         $("#result").html("Computer wins"); 
+    }
+    else  if(computerChoice > .555 && userChoice==="scissors") {
+         $("#result").html("User wins!"); 
+    }
+     else  if(computerChoice > .555 && userChoice==="paper") {
+        $("#result").html("There is a tie"); 
+    }
+    else  if(computerChoice > .555 && userChoice==="rock") {
+        $("#result").html("Computer wins!"); 
+    } 
+     else  if(computerChoice < .555 && userChoice==="rock") {
+        $("#result").html("User wins!"); 
+    } 
+     else  if(computerChoice < .555 && userChoice==="scissors") {
+        $("#result").html("There is a tie"); 
+    } 
+     else  if(computerChoice < .555 && userChoice==="paper") {
+        $("#result").html("Computer wins!"); 
+    } 
+});
+
 
 var randomNumber;
 randomNumber= 0;
